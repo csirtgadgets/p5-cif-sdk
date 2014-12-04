0e6f200beaa02379c3de5eefb6d7dd751b931425
Wes Young <wes@barely3am.com>
2014-12-04 14:45:15 -0500

    fixes #11

--- lib/CIF/SDK.pm
+++ lib/CIF/SDK.pm
@@ -14,7 +14,7 @@ CIF::SDK - The CIF Software Development Kit
 
 =cut
 
-our $VERSION        = '0.00_12';
+our $VERSION        = '0.00_13';
 our $API_VERSION    = 2;
 
 =head1 VERSION
--- lib/CIF/SDK/Format/Bro.pm
+++ lib/CIF/SDK/Format/Bro.pm
@@ -3,8 +3,53 @@ package CIF::SDK::Format::Bro;
 use strict;
 use warnings;
 
-sub understands {}
+use Mouse;
+use Regexp::Common qw/net/;
+use Regexp::Common qw /URI/;
+use Regexp::Common::net::CIDR ();
 
-sub process {}
+with 'CIF::SDK::Format';
+
+use Data::Dumper;
+
+use constant type_hash => {
+    'ipv4'  => 'ADDR',
+    'url'   => 'URL',
+    'fqdn'  => 'DOMAIN'
+};
+    
+sub understands {
+    my $self = shift;
+    my $args = shift;
+
+    return unless($args->{'format'});
+    return 1 if($args->{'format'} eq 'bro');
+}
+
+sub process {
+    my $self = shift;
+    my $data = shift;
+
+    my @text = ("#fields\tindicator\tindicator_type\tmeta.desc\tmeta.cif_confidence\n");
+    
+    
+    foreach my $d (@$data){
+        
+        my @array;
+        foreach my $c (('observable','otype','tags','confidence')){
+            
+            my $x = $d->{$c} || '-';
+            if($c eq 'otype'){
+                $x = 'Intel::'.type_hash->{$d->{$c}};
+            }
+            $x = join('|',@$x) if(ref($x) eq 'ARRAY');
+            push(@array,$x);
+        }
+        push(@text,join("\t",@array));
+    }
+    return join("\n",@text);
+}
+
+__PACKAGE__->meta()->make_immutable();
 
 1;
