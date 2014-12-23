22aa96fe9f6ed859ec89637de484bc800122a874
Wes Young <wes@barely3am.com>
2014-12-23 15:10:04 -0500

    default cols fix...

--- lib/CIF/SDK/Format.pm
+++ lib/CIF/SDK/Format.pm
@@ -8,7 +8,7 @@ use Mouse::Role;
 requires qw/understands process/;
 
 use constant DEFAULT_COLS   => [ 
-    'tlp','group','reporttime','lasttime','observable','cc','confidence','tags','rdata','provider'
+    'tlp','group','reporttime','observable','cc','confidence','tags','rdata','provider'
 ];
 
 has 'columns'   => (
