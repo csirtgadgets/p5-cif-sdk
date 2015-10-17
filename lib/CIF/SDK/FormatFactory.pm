a3429a3255533fb0ed644c186f6947f3a9e58d4a
Wes Young <wes@barely3am.com>
2015-10-17 09:39:28 -0400

    fixes #44

--- lib/CIF/SDK/FormatFactory.pm
+++ lib/CIF/SDK/FormatFactory.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 
 use Module::PluginFinder;
+use Try::Tiny;
+use Carp qw/croak/;
 
 my $finder = Module::PluginFinder->new(
     search_path => ['CIF::SDK::Format'],
@@ -15,7 +17,18 @@ my $finder = Module::PluginFinder->new(
 
 sub new_plugin {
     my ($self,$args) = @_;
-    return $finder->construct($args,%{$args}) or die "I don't know how to create this type of Plugin";
+    my $ret;
+    try{
+        $ret = $finder->construct($args,%{$args}) or die "I don't know how to create this type of Plugin";
+    } catch {
+        $ret = shift;
+        if($ret =~ /Unable to find/){
+            $ret = 0;
+        } else {
+            croak($ret);
+        }
+    };
+    return $ret;
 }
 
 1;
